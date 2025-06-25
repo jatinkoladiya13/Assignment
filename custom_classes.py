e784c7a1e6537c878a9396ee59a9f5578adc66d8
jatin <jatinkoladiya130@gmail.com>
2025-06-25 20:10:03 +0530

    first commit

---
+++ custom_classes.py
@@ -0,0 +1,25 @@
+class Rectangle:
+
+    def __init__(self, length: int, width:int):
+        self.length = length
+        self.width = width
+        self._index = 0
+
+    def __iter__(self):
+        self._index = 0
+        return self
+
+    def __next__(self):
+        if self._index == 0:
+            self._index += 1
+            return {'length': self.length}
+        elif self._index == 1:
+            self._index += 1
+            return {'width': self.width}
+        else:
+            raise StopIteration    
+        
+
+rectangle = Rectangle(15,7)
+for item in rectangle : 
+    print(item)     
